e29fad5303a4760b8e8d34c4e660a265d0524d0c
Swarom <swarom66@gmail.com>
2026-02-16 19:33:49 +0530

    Stop tracking test_gradcam.py

--- Backend/tests/test_gradcam.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-# Initialize client
-client = genai.Client(api_key="AIzaSyB-TW2N6_tVPeHnhZl9C-3BAGNXdWsJEb8")
-
-# List models
-models = client.models.list()
-for model in models:
-    print(model.name)
